6e3cbc1d0b84da0fb1913308c3242e27e69f7574
Maciej Abramowski <m.abramowski@wp.pl>
2017-06-10 12:29:27 +0200

    if, switch

--- js/7-conditions.js.js
+++ js/7-conditions.js.js
@@ -1,3 +1,40 @@
 'use strict';
 
+var wzrostMateusza = 190;
+var wzrostOlgi = 170;
+
+if ( wzrostOlgi > wzrostMateusza) {
+    console.log( 'Olga jest wyższa');
+} else {
+    console.log('Olga jest niższa');
+}
+
+
+if ( wzrostOlgi > wzrostMateusza) {
+    console.log( 'Olga jest wyższa');
+} else if ( wzrostOlgi == wzrostMateusza) {
+    console.log("Olga jest tak wysoka jak Mateusz");
+}
+else {
+    console.log('Olga jest niższa');
+}
+
+
+//switch
+
+var kolor = 'zielony';
+
+switch (kolor) {
+    case 'czerwony':
+        console.log('Kolor czerwony');
+        break;
+    case 'zielony':
+        console.log('Kolor zielony');
+        break;
+    case 'niebieski':
+        console.log('Kolor niebieski');
+        break;
+    default:
+        console.log('Inny kolor');
+             }
 
